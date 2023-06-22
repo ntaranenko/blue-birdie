ee10ec28b1f04edc2804544143c07ce7acf6a2d0
Nikita Taranenko <soomraq.x@gmail.com>
2023-06-22 13:47:38 +0300

    ADD: basic project theme

---
+++ src/styles/theme.js
@@ -0,0 +1,30 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    black: "#000000",
+    accent: "#5CD3A8",
+    primaryColor: "#EBD8FF",
+    primaryButtonTextColor: "#373737",
+    userCardBackground:
+      "linear-gradient(142deg, #471CA9 0%, #5736A3 69.10%, #4B2A99 100%)",
+  },
+  fs: {
+    s: "18px",
+    m: "20px",
+    l: "26px",
+  },
+  fw: {
+    medium: 500,
+    semiBold: 600,
+  },
+  borders: {},
+  shadows: {
+    userCardShadow:
+      "-2.5776965618133545px 6.873857021331787px 20.621572494506836px 0px rgba(0, 0, 0, 0.23)",
+    decorShadow:
+      "0px 3.4369285106658936px 2.5776965618133545px 0px #FBF8FF inset, 0px 3.4369285106658936px 3.4369285106658936px 0px rgba(0, 0, 0, 0.06), 0px -1.7184642553329468px 3.4369285106658936px 0px #AE7BE3 inset",
+  },
+  buttonShadow:
+    "0px 3.4369285106658936px 3.4369285106658936px 0px rgba(0, 0, 0, 0.25)",
+  textShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+};
